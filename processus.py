94e5f7e0398eb823adda39adfe206e36fafbc9be
goujonbe <b.a.goujon@gmail.com>
2016-04-21 20:44:07 +0200

    apport de la fonctionnalité acheter des actions et documentation

--- processus.py
+++ processus.py
@@ -1,7 +1,44 @@
 
 
 from tkinter import *
+from tkinter.messagebox import showinfo
 import random
+
+
+
+# ----- Fonctions : -----
+
+def afficher_confirmation() :
+    """fonction créant une petite interface graphique confirmant l'achat"""
+
+    showinfo("Confirmation de l'achat",
+             "Vous avez bien fait l'acquisition de vos actions")
+
+
+def ouvrir() :
+    """création d'une nouvelle interface pour l'achat d'actions"""
+
+    fen = Tk()
+    fen.title("Achat des actions")
+
+    explication = Label(fen, text = "Nombre d'actions souhaitées : ")
+
+    actions = IntVar()
+
+    champ = Entry(fen, textvariable = actions)
+    champ.focus_set()
+    
+    bouton = Button(fen, text = 'Valider', command = afficher_confirmation)
+
+    # disposition de tous les widgets créés à l'aide de la méthode grid()
+    explication.grid(row = 1, column = 1, padx = 10, pady = 10)
+    champ.grid(row = 1, column = 2, padx = 10, pady = 10)
+    bouton.grid(row = 1, column = 3, padx = 10, pady = 10)
+    
+    fen.mainloop()
+    
+    return actions.get() # idée : envoyer à une fonction stocker
+    
 
 def tracer_segments() :
     """fonction qui permet le tracé d'un segment aléatoire"""
@@ -12,14 +49,15 @@ def tracer_segments() :
     x_1 = pt[0] + T*10
     y_1 = pt[1] + random.randint(-10,10)
     
-    canevas.create_line(pt[0], pt[1], x_1, y_1)
-    pt = (x_1, y_1)
+    # utilisation de la méthode create_line() appliquée à l'objet canevas :
+    canevas.create_line(pt[0], pt[1], x_1, y_1, fill = 'blue')
+    
+    pt = (x_1, y_1) # mise à jour du point d'arrivée d'où on repartira
 
+    # utilisation de la méthode after() appliquée à l'objet fenetre.
+    # Elle permet d'exécuter une action après un temps donné en ms.
     fenetre.after(T*1000, tracer_segments)
 
-
-        
-    
 
 def tracer_axes(T) :
     """trace le repère en fonction de la période puis fait appel
@@ -32,7 +70,7 @@ def tracer_axes(T) :
     # axe des ordonnées
     canevas.create_line(10, 395, 10, 10, arrow = LAST)
 
-    # graduation des axes
+    # graduation des axes :
 
     # axe des abscisses (en fonction de la période T)
     abscisse = 10
@@ -47,18 +85,18 @@ def tracer_axes(T) :
         ordonnee = ordonnee + 20
 
     
-        
-
 
 
 def demarrer() :
     """fonction répondant à l'utilisateur lorsqu'il appuie sur le bouton
-    démarrer. Permet le démarrage du tracé."""
+    démarrer. Permet le démarrage du tracé (des axes et de la courbe)."""
 
     global T
     tracer_axes(T)
     tracer_segments()
 
+
+# ----- Procédure : -----
 
 pt = (10, 200)
 
@@ -67,13 +105,19 @@ T = int(input("Période souhaitée (en s) : "))
 fenetre = Tk()
 fenetre.title("Cours de l'action")
 
+# initialisation de l'objet canevas à partir de la classe Canvas() :
 canevas = Canvas(fenetre, width = 1000, height = 400, bg = 'white')
-canevas.grid(row = 1, column = 1, rowspan = 3, sticky = W, padx = 10, pady = 10)
+# affichage :
+canevas.grid(row = 1, column = 1, rowspan = 3, padx = 10, pady = 10)
 
+# initialisation d'objets de classe Button() :
 bouton1 = Button(fenetre, text = 'Démarrer', command = demarrer)
 bouton2 = Button(fenetre, text = 'Quitter', command = fenetre.destroy)
-
+bouton3 = Button(fenetre, text = 'Acheter des actions', command = ouvrir)
+# affichage :
 bouton1.grid(row = 1, column = 2, padx = 10, pady = 10)
 bouton2.grid(row = 3, column = 2, padx = 10, pady = 10)
+bouton3.grid(row = 2, column = 2, padx = 10, pady = 10) 
 
+# démarrage du réceptionnaire d'évènements associé à la fenêtre
 fenetre.mainloop()
