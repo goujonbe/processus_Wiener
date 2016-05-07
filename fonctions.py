077e5cda76bba97ab16a4cb573fb1b4b4eaa42cb
goujonbe <b.a.goujon@gmail.com>
2016-05-07 19:03:38 +0200

    fichier contenant la fonction de stockage des données

--- fonctions.py
+++ fonctions.py
@@ -1,65 +1,17 @@
+def stocker(donnee) :
+    """L'objectif de cette fonction est de stocker sous forme de liste
+    toutes les valeurs prises par la courbe. (ce n'est pas exactement toutes
+    les valeurs mais seulement les points de changement de courbure)."""
 
-from tkinter import *
-import random
+    # data stocke toutes les données qui sont envoyées à la fonction
+    # il s'agit d'une liste de tuples
+    global data
 
-
-
-def tracer_segments(T) :
-    """trace un segment aléatoire"""
-
-    global pt
-
-    # affichage des axes :
-
-    # axe des abscisses
-    canevas.create_line(5, 390, 390, 390, arrow = LAST)
-    # axe des ordonnées
-    canevas.create_line(10, 395, 10, 10, arrow = LAST)
-
-    # graduation des axes
-
-    # axe des abscisses (en fonction de la période T)
-    abscisse = 10
-    while abscisse <= 380 :
-        canevas.create_line(abscisse, 388, abscisse, 392)
-        abscisse = abscisse + T
-
-    # axe des ordonnées
-    ordonnee = 30
-    while ordonnee <= 390 :
-        canevas.create_line(8, ordonnee, 12, ordonnee)
-        ordonnee = ordonnee + 20
-
-    for i in range (0,200) :
-
-        x_1 = pt[0] + T
-        y_1 = pt[1] + random.randint(-10,10)
-        
-        canevas.create_line(pt[0], pt[1], x_1, y_1)
-        pt = (x_1, y_1)
-
-def demarrer() :
-    """fonction répondant à l'utilisateur lorsqu'il appuie sur le bouton
-    démarrer. Permet le démarrage du tracé."""
-  
-    global T
-    tracer_segments(T)
+    data.append(donnee)
+    return data
 
 
-pt = (10, 200)
 
-T = int(input("Période souhaitée : "))
+data = [(10, 200)]
 
-fenetre = Tk()
-fenetre.title("Processus de Wiener")
 
-canevas = Canvas(fenetre, width = 400, height = 400, bg = 'white')
-canevas.grid(row = 1, column = 1, rowspan = 3, sticky = W, padx = 10, pady = 10)
-
-bouton1 = Button(fenetre, text = 'Démarrer', command = demarrer)
-bouton2 = Button(fenetre, text = 'Quitter', command = fenetre.destroy)
-
-bouton1.grid(row = 1, column = 2, padx = 10, pady = 10)
-bouton2.grid(row = 3, column = 2, padx = 10, pady = 10)
-
-fenetre.mainloop()
